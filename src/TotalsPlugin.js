38882472f63796c164622841c9d80eab2db7822e
Nick Payne <slashnick@gmail.com>
2013-09-04 21:47:11 +0100

    Add support for vertical scrollbar and reordering columns.

--- src/TotalsPlugin.js
+++ src/TotalsPlugin.js
@@ -5,47 +5,66 @@ TotalsPlugin.prototype._scrollOffset = 0;
 
 TotalsPlugin.prototype._rowHeight = 0;
 
+TotalsPlugin.prototype._$totalsViewport = null;
+
+TotalsPlugin.prototype._$totals = null;
+
 TotalsPlugin.prototype.init = function (grid) {
-  console.log('initialise plugin', arguments);
   this._grid = grid;
-  this._rowHeight = grid.getOptions().rowHeight;;
+  this._rowHeight = grid.getOptions().rowHeight;
+
+  var viewport = grid.getCanvasNode().parentElement;
+  var width = viewport.offsetWidth;
+  if (viewport.scrollHeight > viewport.offsetHeight) {
+    width -= $.getScrollbarWidth();
+  }
+
+  var style = 'top: -' + this._rowHeight + 'px; width: ' + width + 'px;';
+  var totalsViewport = '<div style="' + style + '" class="slick-viewport totals-viewport">';
+  viewport.insertAdjacentHTML('afterend', totalsViewport);
+  this._$totalsViewport = $('.totals-viewport');
   this._appendTotalsRow(grid);
+
   grid.onColumnsResized.subscribe(this._handleColumnsResized.bind(this));
+  grid.onColumnsReordered.subscribe(this._handleColumnsReordered.bind(this));
   grid.onScroll.subscribe(this._handleScroll.bind(this));
 };
 
 TotalsPlugin.prototype.destroy = function () {
+  this._$totalsViewport.remove();
 };
 
 TotalsPlugin.prototype._appendTotalsRow = function (grid) {
-  var width = grid.getCanvasNode().getBoundingClientRect().width;
+  var width = grid.getCanvasNode().offsetWidth;
   var style = 'top: -' + this._rowHeight + 'px; width: ' + width + 'px; position: relative;';
-  var totalsRow = '<div style="' + style + '" class="ui-widget-content slick-row totals">';
+  var $totalsRow = $('<div style="' + style + '" class="ui-widget-content slick-row totals"></div>');
   var totals = grid.getData().getTotals();
   var columns = grid.getColumns();
+  var $cell;
 
   for (var i = 0, l = columns.length; i < l; i++) {
-    totalsRow += '<div class="slick-cell r' + i + ' l' + i + '">' + totals[columns[i].id] + '</div>';
+    $cell = $('<div class="slick-cell"></div>').addClass('l' + i + ' r' + i).text(totals[columns[i].id]);
+    $totalsRow.append($cell);
   }
 
-  totalsRow += '</div>';
-
-  grid.getCanvasNode().parentElement.insertAdjacentHTML('afterend', totalsRow);
+  this._$totalsViewport.empty().append($totalsRow);
+  this._$totals = $totalsRow;
 };
 
-TotalsPlugin.prototype._handleColumnsResized = function () {
-  var canvasNode = grid.getCanvasNode();
-  var viewportWidth = canvasNode.parentElement.getBoundingClientRect().width;
-  var columnsWidth = canvasNode.getBoundingClientRect().width;
+TotalsPlugin.prototype._handleColumnsResized = function (event, update) {
+  var viewport = update.grid.getCanvasNode().parentElement;
+  var top = (viewport.scrollWidth > viewport.offsetWidth) ? this._rowHeight + $.getScrollbarWidth() : this._rowHeight;
+  this._$totals.width(viewport.scrollWidth);
+  this._$totalsViewport.css('top', top * -1 + 'px')
+};
 
-  var top = (columnsWidth > viewportWidth) ? this._rowHeight - $.getScrollbarWidth() : this._rowHeight;
-  $('.slick-row.totals').width(columnsWidth).css('top', top * -1 + 'px')
-
+TotalsPlugin.prototype._handleColumnsReordered = function(event, update) {
+  this._appendTotalsRow(update.grid);
 };
 
 TotalsPlugin.prototype._handleScroll = function(event, update) {
   if (this._scrollOffset != update.scrollLeft) {
     this._scrollOffset = update.scrollLeft;
-    $('.slick-row.totals').css('left', this._scrollOffset * -1);
+    this._$totals.css('left', this._scrollOffset * -1);
   }
 };
